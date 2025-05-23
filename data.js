979adb48594f3c093253f07d05d89bbc79f27510
E-RKP <edobackup111@gmail.com>
2025-05-23 16:05:14 +0200

    added javascript

--- data.js
+++ data.js
@@ -1,4 +1,12 @@
-    let object = {}
+  const Numberr = document.getElementById('numerone')
+  const button = document.getElementById('button')
 
+  function randomFilm (){
 
-    
+    let numerofilm = Math.floor(Math.random()*1412)
+    Numberr.innerHTML = numerofilm
+  }
+
+  button.addEventListener('click', ()=>{
+    randomFilm();
+  })
